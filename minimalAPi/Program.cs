b3293b8c51dddd2924336ac0450d88805010d724
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-27 10:42:21 -0300

    Aprendendo sobre requisições no AspNet

--- minimalAPi/Program.cs
+++ minimalAPi/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/nome/{nome}", (string nome) => Results.Ok("Hello " + nome));
 
+app.MapPost("/", (User user) =>{
+    return Results.Ok(user);
+});
 app.Run();
+
+public class User {
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}

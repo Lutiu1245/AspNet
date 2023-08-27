06ea2aeadc169e5b7498bdb3f718485874696cb8
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-27 11:30:19 -0300

    Criado Migrations

--- minimalAPi/Program.cs
+++ minimalAPi/Program.cs
@@ -1,14 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/nome/{nome}", (string nome) => Results.Ok("Hello " + nome));
+app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/", (User user) =>{
-    return Results.Ok(user);
-});
 app.Run();
-
-public class User {
-    public int Id { get; set; }
-    public string? Nome { get; set; }
-}
